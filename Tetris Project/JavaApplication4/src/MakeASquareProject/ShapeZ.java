39a5c225c24eda60d5d383d0e4440ae98ec96682
Ahmad Ouda <104653414+3oda@users.noreply.github.com>
2023-12-17 00:34:31 +0200

    Add files via upload

---
+++ Tetris Project/JavaApplication4/src/MakeASquareProject/ShapeZ.java
@@ -0,0 +1,71 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package MakeASquareProject;
+
+/**
+ *
+ * @author RMDN
+ */
+import java.util.ArrayList;
+
+public class ShapeZ extends Shape
+{
+    ArrayList <ArrayList<Pair>> positionCombinations = new ArrayList<>();
+
+    public ShapeZ(){
+        generateCombinations();
+    }
+
+    // generateCombinations, calculates all the valid positions for Shape Z.
+    @Override
+    void generateCombinations() {
+        ArrayList <Pair> tempArray = new ArrayList<>();
+        boolean inBounds;
+
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < 4; j++) {
+                // To generate first rotation of Z;
+                inBounds = Shape.checkBounds(i, j) &&
+                        Shape.checkBounds(i, j+1) &&
+                        Shape.checkBounds(i+1, j+1) &&
+                        Shape.checkBounds(i+1, j+2);
+                if(inBounds) {
+                    tempArray = new ArrayList<>();
+                    tempArray.add(new Pair(i, j));
+                    tempArray.add(new Pair(i, j+1));
+                    tempArray.add(new Pair(i+1, j+1));
+                    tempArray.add(new Pair(i+1, j+2));
+                    positionCombinations.add(tempArray);
+                }
+
+                // To generate second rotation of Z;
+                inBounds = Shape.checkBounds(i, j) &&
+                        Shape.checkBounds(i+1, j) &&
+                        Shape.checkBounds(i+1, j-1) &&
+                        Shape.checkBounds(i+2, j-1);
+                if(inBounds) {
+                    tempArray = new ArrayList<>();
+                    tempArray.add(new Pair(i, j));
+                    tempArray.add(new Pair(i+1, j));
+                    tempArray.add(new Pair(i+1, j-1));
+                    tempArray.add(new Pair(i+2, j-1));
+                    positionCombinations.add(tempArray);
+                }
+            }
+        }
+    }
+    public void printCombinations (){
+        System.out.println(positionCombinations.size());
+        for (ArrayList<Pair> positionCombination : positionCombinations) {
+            for (Pair pair : positionCombination) {
+                int x = pair.x;
+                int y = pair.y;
+                System.out.println(x + " " + y);
+            }
+            System.out.println();
+        }
+    }
+}
+
